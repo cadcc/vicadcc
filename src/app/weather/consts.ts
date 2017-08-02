04f01f02a7ca95a25fea976211a0ba3d144df26f
Eduardo Riveros <eriveros@dcc.uchile.cl>
2017-08-01 21:20:46 -0400

    First version

---
+++ src/app/weather/consts.ts
@@ -0,0 +1,51 @@
+export const CODE_IMAGES = {
+    '0': 'foggy', // Tornado
+    '1': 'raining', // Tropical Storm
+    '2': 'foggy', // Hurricane
+    '3': 'stormy', // Severe Thunderstorms
+    '4': 'stormy', // Thunderstorms
+    '5': 'snowy', // Mixed Rain and Snow
+    '6': 'rainy', // Mixed Rain and Sleet
+    '7': 'snowy', // mixed snow and sleet
+    '8': 'snowy', // Freezing Drizzle
+    '9': 'snowy', // Drizzle
+    '10': 'rainy', // Freezing Rain
+    '11': 'rainy', // Showers
+    '12': 'rainy', // Showers (Again? wtf)
+    '13': 'snowy', // Snow Flurries
+    '14': 'snowy', // Light snow showers
+    '15': 'snowy', // Blowing Snow
+    '16': 'snowy', // Snow
+    '17': 'snowy', // Hail
+    '18': 'snowy', // Sleet
+    '19': 'windy', // Dust
+    '20': 'foggy', // Foggy
+    '21': 'foggy', // Haze
+    '22': 'foggy', // Smoky
+    '23': 'windy', // blustery
+    '24': 'windy', // Windy
+    '25': 'windy', // Cold
+    '26': 'cloudy', // Cloudy
+    '27': 'partial-night', // Mostly Cloudy Night
+    '28': 'partial-day', // Mostly Cloudy Day
+    '29': 'partly-night', // Partly Cloudy Night
+    '30': 'partly-day', // Partly Cloudy Day
+    '31': 'clear-night', // Clear Night
+    '32': 'clear-day', // Sunny
+    '33': 'clear-night', // Fair Night
+    '34': 'clear-day', // Fair Day
+    '35': 'snowy', // Mixed Rain and Hail
+    '36': 'sunny', // Hot
+    '37': 'stormy', // Isolated Thunderstorms
+    '38': 'stormy', // Scattered Thunderstorms
+    '39': 'stormy', // Scattered Thunderstorms
+    '40': 'rainy', // Scattered Showers
+    '41': 'snowy', // Heavy Snow
+    '42': 'snowy', // Scattered Snow Showers
+    '43': 'snowy', // Heavy Snow
+    '44': 'partly-day', // Partly Cloudy
+    '45': 'stormy',  // Thundershowers
+    '46': 'snowy', // Snow Showers
+    '47': 'stormy', // Isolated Thundershowers
+    '3200': 'foggy' // Not available
+};
