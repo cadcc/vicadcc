bce90396094fc9efaf4a7446ca5a4d20d52b31e0
tomyahu <tomyahu@gmail.com>
2018-07-13 13:11:56 -0400

    cambio de proveedor de Yahoo a DarkSky

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -9,26 +9,22 @@ export class WeatherService {
   private temperature = 20;
   private sunrise = {hours: 6, minutes: 0};
   private sunset = {hours: 18, minutes: 0};
+  private DARKSKY_API = 'https://api.cadcc.cl/tiempo/';
 
-  private YAHOO_API = 'https://query.yahooapis.com/v1/public/yql?' +
-    'q=select%20item.condition,astronomy%20from%20weather.forecast%20' +
-    'where%20woeid%20%3D%20349859%20and%20u%20%3D%20%22c%22&format=json' +
-    '&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 
   constructor() { }
 
   public getWeatherData() {
-    fetch(this.YAHOO_API).then((response) => {
+    fetch(this.DARKSKY_API).then((response) => {
       return response.json();
     }).then((data) => {
-      const condition = data.query.results.channel.item.condition;
-      const astronomy = data.query.results.channel.astronomy;
-      this.weatherImage = CODE_IMAGES[condition.code];
-      this.temperature = condition.temp;
-      this.sunrise = { hours: astronomy.sunrise.split(':')[0],
-                       minutes: astronomy.sunrise.split(':')[1].split(' ')[0]};
-      this.sunset = { hours: astronomy.sunset.split(':')[0],
-        minutes: astronomy.sunset.split(':')[1].split(' ')[0]};
+      this.weatherImage = (Object.keys(CODE_IMAGES).indexOf(data.currently.icon) !== -1) ? CODE_IMAGES[data.currently.icon] : 'default';
+      this.temperature = Math.round((data.currently.temperature - 32) * 5 / 9);
+      this.sunrise = { hours: new Date(data.daily.data.sunriseTime * 1000).getHours(),
+                       minutes: new Date(data.daily.data.sunriseTime * 1000).getMinutes()};
+      console.log(data.daily.data);
+      this.sunset = { hours: new Date(data.daily.data.sunsetTime * 1000).getHours(),
+        minutes: new Date(data.daily.data.sunsetTime * 1000).getMinutes()};
     }).catch((ex) => {
       console.error('Error fetching forecast', ex);
     });
